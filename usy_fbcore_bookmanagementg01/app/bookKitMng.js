636b6c1385fbd851b2e184c039e2a874d62d98a7
jilek <daniel.jilek@unicorn.com>
2021-10-15 13:12:20 +0200

    set state fix

--- usy_fbcore_bookmanagementg01/app/bookKitMng.js
+++ usy_fbcore_bookmanagementg01/app/bookKitMng.js
@@ -2,25 +2,15 @@ const {UriBuilder} = require("uu_appg01_core-uri");
 const OidcToken = require("./oidc-interactive-login");
 const {get, post} = require("./calls.js");
 
-async function _setPageState(bookUri, pageCode, state, token) {
-  let commandUri = UriBuilder.parse(bookUri).setUseCase("updatePage").toUri();
-  let dtoIn = {
-    code: pageCode,
-    state: state
-  };
-  await post(commandUri, dtoIn, token);
-}
-
 async function setPageState(bookUri, state, username, password) {
-  console.info(`Setting ${state} state to all pages in book "${bookUri}".`);
+  console.info(`Setting "${state}" state to all pages in book "${bookUri}".`);
   let token = await OidcToken.login(username, password);
   let menu = await _loadMenu(bookUri, token);
   for (const rootPageCode of Object.keys(menu.itemMap)) {
     let selectedPages = _loadPagesUnderRoot(menu, rootPageCode);
-    console.info(`Selected pages: ${selectedPages}`);
     let pagesToSetState = _filterOutByState(selectedPages, state);
-    console.info(`Pages to set state: ${pagesToSetState}`);
     for (const page of pagesToSetState) {
+      console.info(`Setting "${state}" state to page ${page.code}`);
       await _setPageState(bookUri, page.code, state, token);
     }
   }
@@ -37,9 +27,8 @@ async function _loadMenu(bookUri, token) {
 function _loadPagesUnderRoot(bookMenu, rootPage) {
   let currentPage = bookMenu.itemMap[rootPage];
   let rootIndent = currentPage.indent;
-  console.info(`Current page: ${JSON.stringify(currentPage)}`);
   //always add the root page since it won't be added due to its indent
-  let result = [{page: rootPage, state: currentPage.state}];
+  let result = [{code: rootPage, state: currentPage.state}];
   if (currentPage.next) {
     result.push(..._loadSubPages(bookMenu, currentPage.next, rootIndent + 1));
   }
@@ -52,7 +41,7 @@ function _loadSubPages(bookMenu, page, minIndent) {
   if (currentPage.indent < minIndent) {
     return [];
   }
-  result.push({page: page, state: currentPage.state});
+  result.push({code: page, state: currentPage.state});
   if (currentPage.next) {
     let nextPages = _loadSubPages(bookMenu, currentPage.next, minIndent);
     result.push(...nextPages);
@@ -70,21 +59,19 @@ function _filterOutByState(pages, state) {
   }
   return result;
 }
+
+async function _setPageState(bookUri, pageCode, state, token) {
+  let commandUri = UriBuilder.parse(bookUri).setUseCase("updatePage").toUri();
+  let dtoIn = {
+    code: pageCode,
+    state: state
+  };
+  await post(commandUri, dtoIn, token);
+}
+
 async function updateFulltextIndex(bookUri, token) {
   let commandUri = UriBuilder.parse(bookUri).setUseCase("updateBookIndex").toUri();
   await post(commandUri, {}, token);
-}
-
-class BookPage {
-
-  constructor(pageCode, details) {
-    this.code = pageCode;
-    this.state = details.state;
-  }
-
-  toString() {
-    return `Code: ${this.code}, state: ${this.state}`;
-  }
 }
 
 module.exports = setPageState;
